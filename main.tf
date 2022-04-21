18b3be1508c196cf69a2c3d52bcc59ae03b7fdd8
Sokren <remi.salandre@gmail.com>
2022-04-21 10:42:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-module "webVM" {
-  source  = "app.terraform.io/salandre-co/webVM/azure"
-  version = "~> 1.0.5"
+module "moduledemo-website" {
+  source  = "app.terraform.io/salandre-co/moduledemo-website/azure"
+  version = "~> 0.0.1"
   rg_location = var.locationRg
   loc = var.locationName
   instance_type = var.instance
   app_environment = var.environment
+  linux_admin_password = var.linux_password
 }
-
-#module "domain" {
-#  for_each = var.locationName
-#  source  = "app.terraform.io/salandre-co/domain/ovh"
-#  version = "1.0.1"
-#  ip_address = module.webVM.*.ip-public
-#  subdomain = each.value
-#}
