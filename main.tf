5ac03714ed5b4ab897c54f119cd43bd37d1974a8
Sokren <remi.salandre@gmail.com>
2022-05-30 14:16:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "moduledemo-website" {
-  source  = "app.terraform.io/salandre-co/moduledemo-website/azure"
+  source  = "app.terraform.io/rs-tfc-yt/moduledemo-website/azure"
   version = "~> 0.0.1"
   rg_location = var.locationRg
   loc = var.locationName
